8ac7283cb358f3af37b35551c73a992f6efdce00
Aditi Joshi <aditijoshi@aditismacintosh.olin.edu>
2016-09-23 10:38:01 -0400

    adding work from Friday

--- read2D/read2D.ino
+++ read2D/read2D.ino
@@ -6,6 +6,9 @@ int servoPin = 11;
 const int analogInPin = A0;
 int sensorVals[180];
 int sensorValue;
+int sensorValue1;
+int sensorValue2;
+int sensorValue3;
 int calibratedValue;
 int distance;
 
@@ -17,31 +20,37 @@ void setup() {
 void loop() {
   // Serial.write("test\n");
   
-  for (pos = 0; pos <= 90; pos += 1) { //test what 90 degrees we really want 
+  for (pos = 30; pos <= 70; pos += 1) { //test what 90 degrees we really want 
     upDownServo.write(pos);
-    delay(15);
-    sensorValue = analogRead(analogInPin);
-    calibratedValue = map(sensorValue, 0, 1023, 0, 255);
-    sensorVals[pos] = calibratedValue;
+    delay(240);
+    sensorValue1 = analogRead(analogInPin);
+    sensorValue2 = analogRead(analogInPin);
+    sensorValue3 = analogRead(analogInPin);
+    sensorValue = (sensorValue1 + sensorValue2 + sensorValue3)/3;
+    //calibratedValue = map(sensorValue, 0, 1023, 0, 255);
+    //sensorVals[pos] = calibratedValue;
     //Serial.print("sensorValue");
     Serial.print(sensorValue);
     Serial.print(';');
     //Serial.print("pos");
-    Serial.print(pos);
+    Serial.print(pos-45);
     Serial.print('\n');
   }
 
-  for (pos = 90; pos >= 0; pos -= 1) { //test what 90 degrees we really want 
+  for (pos = 70; pos >= 30; pos -= 1) { //test what 90 degrees we really want 
     upDownServo.write(pos);
-    delay(15);
-    sensorValue = analogRead(analogInPin);
-    calibratedValue = map(sensorValue, 0, 1023, 0, 255);
-    sensorVals[pos] = calibratedValue;
+    delay(240);
+    sensorValue1 = analogRead(analogInPin);
+    sensorValue2 = analogRead(analogInPin);
+    sensorValue3 = analogRead(analogInPin);
+    sensorValue = (sensorValue1 + sensorValue2 + sensorValue3)/3;
+    //calibratedValue = map(sensorValue, 0, 1023, 0, 255);
+    //sensorVals[pos] = calibratedValue;
     //Serial.print("sensorValue");
     Serial.print(sensorValue);
     Serial.print(';');
     //Serial.print("pos");
-    Serial.print(pos);
+    Serial.print(pos-45);
     Serial.print('\n');
   }
   
